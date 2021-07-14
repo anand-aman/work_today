87016f6c9bb7133de6819c4286bb1e658b383c60
Aman Anand <itsamananand@gmail.com>
2021-07-14 12:47:11 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Dartist',
       home: Check(
         isdark: this.isdark,
