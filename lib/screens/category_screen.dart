aafc17851ff9cea99820a970800260a2aa43793f
Ramesh MS <msramesh2k1@gmail.com>
2021-10-04 15:28:23 +0530

    Category Page & Workers Homepage

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,16 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:work_today/components/category_card.dart';
 import 'package:work_today/components/my_button.dart';
 import 'package:work_today/model/category.dart';
 import 'package:work_today/screens/available_worker.dart';
-import 'package:work_today/screens/home_screen.dart';
-import 'package:work_today/screens/worker_home.dart';
 import 'package:work_today/services/check.dart';
 import 'package:work_today/services/firebase_user.dart';
-import 'package:flutter/material.dart';
-import 'package:work_today/components/category_card.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CategoryScreen extends StatefulWidget {
-
   final bool isHirer;
   final bool isdark;
 
@@ -22,17 +18,29 @@ class CategoryScreen extends StatefulWidget {
 
 class _CategoryScreenState extends State<CategoryScreen> {
   Category category = Category();
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: widget.isdark?Colors.grey[850]:Colors.grey,
-      body: SafeArea(
+      appBar: AppBar(
+        elevation: 0,
+        iconTheme:
+            IconThemeData(color: widget.isdark ? Colors.white : Colors.black),
+        title: Text(
+          "Category",
+          style: TextStyle(
+              color: widget.isdark ? Colors.white : Colors.black,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1),
+        ),
+        backgroundColor: widget.isdark ? Colors.black87 : Colors.white,
+      ),
+      body: Container(
+        color: widget.isdark ? Colors.black87 : Colors.white,
         child: Stack(
           children: [
             GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
               itemBuilder: (BuildContext context, int index) {
                 bool isSelected = false;
                 return CategoryCard(
@@ -40,19 +48,19 @@ class _CategoryScreenState extends State<CategoryScreen> {
                   text: category.categoryList[index],
                   isSelected: category.categoryBool[index],
                   onPress: () {
-
-                    if(widget.isHirer){
+                    if (widget.isHirer) {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
                             builder: (context) => AvailableWorker(
                               isdark: widget.isdark,
-                              category: category.categoryList[index],),
+                              category: category.categoryList[index],
+                            ),
                           ));
-                    }
-                    else{
+                    } else {
                       setState(() {
-                        category.categoryBool[index] = !category.categoryBool[index];
+                        category.categoryBool[index] =
+                            !category.categoryBool[index];
                       });
                     }
                   },
@@ -60,26 +68,31 @@ class _CategoryScreenState extends State<CategoryScreen> {
               },
               itemCount: category.categoryList.length,
             ),
-            widget.isHirer ? Container() :Positioned(
-              left: MediaQuery.of(context).size.width * 0.5 - 50,
-              child: MyButton(
-                text: 'Continue..',
-                width: 100.0,
-                onPressed: () async {
-                  await FirebaseCurrentUser().addCategory(category);
-                  await FirebaseCurrentUser().updateCategory(category).then((value) {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Check(
-                            isdark: widget.isdark,
-                          ),
-                        ));
-                  });
-                },
-              ),
-              bottom: 20.0,
-            ),
+            widget.isHirer
+                ? Container()
+                : Positioned(
+                    left: MediaQuery.of(context).size.width * 0.5 + 50,
+                    child: MyButton(
+                      buttonColor: Colors.black,
+                      text: 'Continue..',
+                      width: 140.0,
+                      onPressed: () async {
+                        await FirebaseCurrentUser().addCategory(category);
+                        await FirebaseCurrentUser()
+                            .updateCategory(category)
+                            .then((value) {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Check(
+                                  isdark: widget.isdark,
+                                ),
+                              ));
+                        });
+                      },
+                    ),
+                    bottom: 20.0,
+                  ),
           ],
         ),
       ),
--- lib/screens/worker_home.dart
+++ lib/screens/worker_home.dart
@@ -1,17 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:work_today/components/request_card.dart';
 import 'package:work_today/model/request.dart';
 import 'package:work_today/screens/ownProfile.dart';
-import 'package:work_today/screens/userprofile.dart';
 import 'package:work_today/services/firebase_user.dart';
-import 'package:flutter/material.dart';
+
 import 'home_screen.dart';
 
 final _firestore = FirebaseFirestore.instance;
 
 class WorkerHome extends StatefulWidget {
-   bool isdark;
+  bool isdark;
   WorkerHome({this.isdark});
 
   @override
@@ -22,13 +22,15 @@ class _WorkerHomeState extends State<WorkerHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:this.widget.isdark?Colors.grey[850]: Color(0xFFF6F6F6),
+      backgroundColor:
+          this.widget.isdark ? Colors.grey[850] : Color(0xFFF6F6F6),
       appBar: AppBar(
-        backgroundColor:this.widget.isdark?Colors.grey[850]: Color(0xFFF6F6F6),
+        backgroundColor:
+            this.widget.isdark ? Colors.grey[850] : Color(0xFFF6F6F6),
         elevation: 0.0,
         leading: Padding(
           padding: const EdgeInsets.only(
-            left: 18.0,
+            left: 10.0,
             top: 12.0,
             bottom: 12.0,
             right: 12.0,
@@ -36,42 +38,55 @@ class _WorkerHomeState extends State<WorkerHome> {
           child: IconButton(
             icon: Icon(
               Icons.notifications,
-              size: 25.0,
-              color: this.widget.isdark?Colors.white:Colors.black,
+              size: 25,
+              color: this.widget.isdark ? Colors.white : Colors.black,
             ),
             onPressed: () {},
           ),
         ),
         actions: <Widget>[
           Padding(
-            padding: const EdgeInsets.all(3.0),
+            padding: const EdgeInsets.only(right: 10.0),
             child: GestureDetector(
-
-              child:Icon(Icons.account_circle_outlined,color: Colors.deepPurple,size: 30.0, ) ,
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ownProfile(
-                  isdark: widget.isdark,
-                )));
+              child: Icon(
+                Icons.account_circle_outlined,
+                color: widget.isdark ? Colors.white : Colors.black,
+                size: 25,
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ownProfile(
+                              isdark: widget.isdark,
+                            )));
               },
             ),
           ),
           Padding(
-          padding: const EdgeInsets.all(3.0),
-          child: GestureDetector(
-
-            child:Icon(Icons.brightness_4_outlined,color: Colors.deepPurple,size: 30.0, ) ,
-            onTap: (){
-              setState(() {
-                widget.isdark = !widget.isdark;
-              });
-            },
+            padding: const EdgeInsets.only(right: 10.0),
+            child: GestureDetector(
+              child: Icon(
+                Icons.brightness_4_outlined,
+                color: widget.isdark ? Colors.white : Colors.black,
+                size: 25,
+              ),
+              onTap: () {
+                setState(() {
+                  widget.isdark = !widget.isdark;
+                });
+              },
+            ),
           ),
-        ),
-
-//          Icon(Icons.supervised_user_circle, size: 25.0, color: Colors.black),
-
-          FlatButton(
-              onPressed: () {
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: GestureDetector(
+              child: Icon(
+                Icons.logout,
+                color: widget.isdark ? Colors.white : Colors.black,
+                size: 25,
+              ),
+              onTap: () {
                 FirebaseCurrentUser().signOut();
                 Navigator.pushReplacement(
                     context,
@@ -82,10 +97,8 @@ class _WorkerHomeState extends State<WorkerHome> {
                       settings: RouteSettings(name: 'Home Screen'),
                     ));
               },
-              child: Text('Logout',
-                style: TextStyle(
-              color: this.widget.isdark?Colors.white:Colors.black,
-              ),)),
+            ),
+          ),
         ],
       ),
       body: Container(
@@ -96,18 +109,29 @@ class _WorkerHomeState extends State<WorkerHome> {
             SizedBox(
               height: 25.0,
             ),
-            Text("Hii ${FirebaseCurrentUser.appUser.name},\nHere Are Your Requests",
-                style: TextStyle(
-                    fontFamily: 'FV_ALMELO',
-
-                    fontSize: 23,
-                    fontWeight: FontWeight.w900,
-                    wordSpacing: 2.5,
-                    color: this.widget.isdark?Colors.white:Colors.black)),
-            SizedBox(
-              height: 45.0,
+            Text(
+              "Hello ${FirebaseCurrentUser.appUser.name} ,",
+              style: GoogleFonts.nunito(
+                textStyle: Theme.of(context).textTheme.headline4,
+                fontSize: 24,
+                color: this.widget.isdark ? Colors.white : Colors.black,
+                fontWeight: FontWeight.w700,
+              ),
             ),
-            Expanded(child: RequestStream(
+            Text(
+              "Here are yours request",
+              style: GoogleFonts.nunito(
+                textStyle: Theme.of(context).textTheme.headline4,
+                fontSize: 18,
+                color: this.widget.isdark ? Colors.white : Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Expanded(
+                child: RequestStream(
               isdark: widget.isdark,
             )),
           ],
@@ -117,7 +141,6 @@ class _WorkerHomeState extends State<WorkerHome> {
   }
 }
 
-
 class RequestStream extends StatefulWidget {
   final bool isdark;
   RequestStream({this.isdark});
@@ -126,52 +149,57 @@ class RequestStream extends StatefulWidget {
 }
 
 class _RequestStreamState extends State<RequestStream> {
-
-
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(stream: _firestore.collection("users").doc(FirebaseCurrentUser.user.uid).collection("request").snapshots(),builder: (context, snapshot) {
-      if (!snapshot.hasData) {
-        return Center(
-          child: CircularProgressIndicator(
-            backgroundColor: Colors.lightBlueAccent,
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore
+          .collection("users")
+          .doc(FirebaseCurrentUser.user.uid)
+          .collection("request")
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+
+        final requests = snapshot.data.docs.reversed;
+        List<Widget> requestCardList = [];
+        for (var request in requests) {
+          var data = request.data();
+
+          // if(!availableUserID.contains(docID))
+          //   continue;
+
+          RequestHirer hirerRequest = RequestHirer(
+            hirerName: data['hirername'],
+            hirerID: data['hirerid'],
+            requestID: data['requestid'],
+            location: data['location'],
+            job: data['job'],
+            isAccepted: data['isAccepted'],
+            amount: data['offer'],
+            email: data['email'],
+            phoneNo: data['phoneNo'],
+          );
+
+          requestCardList.add(WorkerRequestCard(
+            isdark: widget.isdark,
+            hirerRequest: hirerRequest,
+          ));
+        }
+
+        return Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          child: ListView(
+            children: requestCardList,
           ),
         );
-      }
-
-      final requests = snapshot.data.docs.reversed;
-      List<Widget> requestCardList = [];
-      for (var request in requests) {
-        var data = request.data();
-
-        // if(!availableUserID.contains(docID))
-        //   continue;
-
-        RequestHirer hirerRequest = RequestHirer(
-          hirerName: data['hirername'],
-          hirerID: data['hirerid'],
-          requestID: data['requestid'],
-          location: data['location'],
-          job: data['job'],
-          isAccepted: data['isAccepted'],
-          amount: data['offer'],
-          email: data['email'],
-          phoneNo: data['phoneNo'],
-        );
-
-        requestCardList.add(WorkerRequestCard(
-          isdark: widget.isdark,
-          hirerRequest: hirerRequest,));
-      }
-
-
-      return Container(
-        alignment: Alignment.center,
-        width: double.infinity,
-        child: ListView(
-          children: requestCardList,
-        ),
-      );
-    },);
+      },
+    );
   }
 }
