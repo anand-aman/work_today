2b0fb4d2131ecd8b20c5e22e2b2eab0e16debaef
Navee Ramesh <naveenarameshvardhini@gmail.com>
2021-10-05 19:14:58 +0530

    Hire_Home_Work_Home_Profile

--- lib/screens/ownProfile.dart
+++ lib/screens/ownProfile.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:work_today/components/my_button.dart';
 import 'package:work_today/services/firebase_user.dart';
+
+import 'home_screen.dart';
 
 class ownProfile extends StatefulWidget {
   final bool isdark;
@@ -12,148 +16,149 @@ class _ownProfileState extends State<ownProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: widget.isdark?Colors.grey[900]:Colors.white,
+      backgroundColor: widget.isdark ? Colors.grey[900] : Colors.white,
       appBar: AppBar(
-
-        backgroundColor: widget.isdark?Colors.black12:Colors.grey[900],
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new_outlined,
+            size: 20,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        iconTheme:
+            IconThemeData(color: widget.isdark ? Colors.white : Colors.black),
+        backgroundColor: widget.isdark ? Colors.black12 : Colors.white,
         elevation: 0,
+        actions: [
+          IconButton(
+              onPressed: () {
+                FirebaseCurrentUser().signOut();
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomeScreen(
+                        isdark: this.widget.isdark,
+                      ),
+                      settings: RouteSettings(name: 'Home Screen'),
+                    ));
+              },
+              icon: Icon(
+                Icons.logout_outlined,
+                size: 20,
+                color: widget.isdark ? Colors.white : Color(0xff7f1cff),
+              ))
+        ],
         title: Row(
-
           children: <Widget>[
-            Icon(
-
-                Icons.account_circle,
-                color: Colors.white),
-            Text("User Profile")
+            Text(
+              "User Profile",
+              style: GoogleFonts.nunito(
+                  color: widget.isdark ? Colors.white : Colors.black,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1),
+            )
           ],
-
         ),
       ),
       body: SingleChildScrollView(
         child: Container(
-          color: widget.isdark?Colors.black12: Colors.white,
-          child: Expanded(
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 16),
-                Container(
-                  alignment: Alignment.topCenter,
-
-                  child: CircleAvatar(
-                    radius: 80,
-                    backgroundImage: AssetImage('images/user.png'),
+          color: widget.isdark ? Colors.black12 : Colors.white,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 16),
+              Container(
+                alignment: Alignment.topCenter,
+                child: CircleAvatar(
+                  radius: 75,
+                  backgroundImage: AssetImage('images/user.png'),
+                ),
+              ),
+              SizedBox(height: 16),
+              Container(
+                alignment: Alignment.center,
+                child: Text(
+                  "${FirebaseCurrentUser.appUser.name}",
+                  style: GoogleFonts.nunito(
+                    textStyle: Theme.of(context).textTheme.headline4,
+                    fontSize: 28,
+                    color: this.widget.isdark ? Colors.white : Colors.black,
+                    fontWeight: FontWeight.w500,
                   ),
-
                 ),
-                SizedBox(height: 16),
-                Container(
-                  alignment: Alignment.center,
-                  child: Text("${FirebaseCurrentUser.appUser.name}", style: TextStyle(
-                      color: Color(0xff7f1cff),
-                      fontSize: 32
-                  ),),
-                ),
-                SizedBox(height: 32),
-
-                Card(
-                    color: Colors.black12,
-                    margin:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        color: Color(0xff7f1cff),
-                      ),
-                      title: Text(
-                        "${FirebaseCurrentUser.appUser.email}",
-                        style:
-                        TextStyle(fontFamily: '',
-                            color: Color(0xff7f1cff),
-                            fontSize: 20.0),
-                      ),
-                    )),
-                Card(
-                    color: Colors.black12,
-                    margin:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.phone_android,
-                        color: Color(0xff7f1cff),
-                      ),
-                      title: Text(
-                        "${FirebaseCurrentUser.appUser.phoneNo}",
-                        style:
-                        TextStyle(fontFamily: '',
-                            color: Color(0xff7f1cff),
-                            fontSize: 20.0),
-                      ),
-                    )
-                ),
-                Card(
-                    color: Colors.black12,
-                    margin:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.location_on_rounded,
-                        color: Color(0xff7f1cff),
-                      ),
-                      title: Text(
-                        "${FirebaseCurrentUser.appUser.location}",
-                        style:
-                        TextStyle(fontFamily: '',
-                            color: Color(0xff7f1cff),
-                            fontSize: 20.0),
-                      ),
-                    )
-                ),
-                Card(
-                    color: Colors.black12,
-                    margin:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.person,
-                        color: Color(0xff7f1cff),
-                      ),
-                      title: Text(
-                        FirebaseCurrentUser.appUser.isHirer?"Hirer": "Worker",
-                        style:
-                        TextStyle(fontFamily: '',
-                            color: Color(0xff7f1cff),
-                            fontSize: 20.0),
-                      ),
-                    )
-                ),
-
-                Card(
-                    color: Colors.black12,
-                    margin:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.category_outlined,
-                        color: Color(0xff7f1cff),
-                      ),
-                      title: Text(
-                        "${FirebaseCurrentUser.appUser.category}",
-                        style:
-                        TextStyle(fontFamily: '',
-                            color: Color(0xff7f1cff),
-                            fontSize: 20.0),
-                      ),
-                    )
-                ),
-
-              ],
-            ),
+              ),
+              SizedBox(height: 10),
+              carder(
+                name: FirebaseCurrentUser.appUser.email.toString(),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              carder(
+                name: FirebaseCurrentUser.appUser.phoneNo.toString(),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              carder(
+                name: FirebaseCurrentUser.appUser.location.toString(),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              carder(
+                name: FirebaseCurrentUser.appUser.isHirer ? "Hirer" : "Worker",
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              carder(
+                name: FirebaseCurrentUser.appUser.category.toString(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
           ),
         ),
       ),
-
-
     );
   }
 }
 
+class carder extends StatelessWidget {
+  final String name;
+  final Icon icon;
+  final Function fn;
+  const carder({Key key, this.name, this.icon, this.fn}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        //this.fn;
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: Center(
+            child: Text(
+              this.name,
+              style: GoogleFonts.nunito(
+                textStyle: Theme.of(context).textTheme.headline4,
+                fontSize: 19,
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          height: 45,
+          width: MediaQuery.of(context).size.width - 50,
+          decoration: BoxDecoration(
+              color: Colors.grey[200], borderRadius: BorderRadius.circular(10)),
+        ),
+      ),
+    );
+  }
+}
